063b4d42b1acf832fa64557abeeebec87897f238
palazzo <2007f.palazzo@gmail.com>
2025-01-25 15:39:05 +0100

    added theme toggle

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,16 +2,39 @@
   <v-navigation-drawer expand-on-hover rail rounded="0">
     <v-list nav>
       <v-list-item color="primary" to="/" prepend-icon="mdi-home" title="Home" value="home"></v-list-item>
-
       <v-divider></v-divider>
-
-      <v-list-item color="primary" to="/dozenten" prepend-icon="mdi-account-tie" title="Dozenten" value="dozenten"></v-list-item>
-      <v-list-item color="primary" to="/kurse" prepend-icon="mdi-book-alphabet" title="Kurse" value="kurse"></v-list-item>
+      <v-list-item color="primary" to="/dozenten" prepend-icon="mdi-account-tie" title="Dozenten"
+                   value="dozenten"></v-list-item>
+      <v-list-item color="primary" to="/kurse" prepend-icon="mdi-book-alphabet" title="Kurse"
+                   value="kurse"></v-list-item>
       <v-list-item color="primary" to="/laender" prepend-icon="mdi-flag" title="Länder" value="laender"></v-list-item>
-      <v-list-item color="primary" to="/lehrbetriebe" prepend-icon="mdi-office-building" title="Lehrbetriebe" value="lehrbetriebe"></v-list-item>
-      <v-list-item color="primary" to="/lernende" prepend-icon="mdi-account-school" title="Lernende" value="lernende"></v-list-item>
+      <v-list-item color="primary" to="/lehrbetriebe" prepend-icon="mdi-office-building" title="Lehrbetriebe"
+                   value="lehrbetriebe"></v-list-item>
+      <v-list-item color="primary" to="/lernende" prepend-icon="mdi-account-school" title="Lernende"
+                   value="lernende"></v-list-item>
     </v-list>
+
+    <template v-slot:append>
+      <v-list nav>
+        <v-list-item>
+          <v-btn
+              block
+              :prepend-icon="theme.global.current.value.dark ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+              @click="toggleTheme"
+          >
+          </v-btn>
+        </v-list-item>
+      </v-list>
+    </template>
   </v-navigation-drawer>
 </template>
+
 <script setup lang="ts">
+import {useTheme} from 'vuetify'
+
+const theme = useTheme()
+
+const toggleTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
 </script>
