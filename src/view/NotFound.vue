dc32fff65833a55e89ef64c66a3ea054d7ca13ee
palazzo <2007f.palazzo@gmail.com>
2025-02-19 13:35:07 +0100

    edited NotFound Page

--- src/view/NotFound.vue
+++ src/view/NotFound.vue
@@ -1,18 +1,50 @@
 <template>
-  <v-card min-height="800">
-    <v-card-title>
-      <h1>404 Not Found</h1>
-    </v-card-title>
-    <v-card-text>
-      <iframe src="https://wayou.github.io/t-rex-runner" frameborder="0" scrolling="no" width="100%" height="100%"
-              loading="lazy"></iframe>
-    </v-card-text>
+  <v-card rounded="0" class="d-flex align-center fill-height">
+    <v-empty-state
+        icon="mdi-map-marker-alert"
+        class="w-100"
+    >
+      <template v-slot:headline>
+        <div class="text-h4 mb-4">
+          404 Not Found
+        </div>
+      </template>
+
+      <template v-slot:title>
+        <div class="text-medium-emphasis text-body-1 mb-4">
+          Oops! The page you're looking for doesn't exist. Beat this game to continue.
+        </div>
+      </template>
+
+      <template v-slot:text>
+        <iframe
+            v-if="isMounted"
+            src="https://wayou.github.io/t-rex-runner"
+            frameborder="0"
+            scrolling="no"
+            width="100%"
+            height="100%"
+            loading="lazy"
+        ></iframe>
+      </template>
+    </v-empty-state>
   </v-card>
 </template>
 
-<style>iframe {
-  position: absolute;
+<script setup lang="ts">
+import {onMounted, ref} from 'vue';
+
+const isMounted = ref(false);
+
+onMounted(() => {
+  isMounted.value = true;
+});
+</script>
+
+<style>
+iframe {
   width: 100%;
   height: 100%;
-  z-index: 999;
-}</style>
+  border-radius: 0;
+}
+</style>
